69446a444547a590644e71595de061dfb6a8992e
Dario Ghilardi <darioghilardi@webrain.it>
2015-09-07 21:07:08 +0200

    Extended the chapter 1 notes.

--- chapter_01/chapter-1-notes.scm
+++ chapter_01/chapter-1-notes.scm
@@ -33,3 +33,25 @@ size
   (cond ((< x 0) (- x))
         (else x)))
 (abs2 -4)
+
+;;; sqrt
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+    guess
+    (sqrt-iter (improve guess x) x)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.0001))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+(define (square x) (* x x))
+
+(sqrt 9)
