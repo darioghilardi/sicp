4200e18ae056c0533d6cbca4a105dc6be7a7e368
Dario Ghilardi <darioghilardi@webrain.it>
2015-09-07 18:19:45 +0200

    Solve the 1.4 exercise.

---
+++ chapter_01/ex-1.4.scm
@@ -0,0 +1,2 @@
+;;; When b is bigger than 0, the if statement evaluates to +, therefore the result of the procedure is a + b
+;;; When b is smaller than 0, the if statement evaluates to -, therefore the result of the procedure is a - b
